4dbc97eb55e6970cd93033a53b1a1eb47d8ca040
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-09-26 15:58:09 +0400

    commit

--- src/_2_DEYISGENLER.java
+++ src/_2_DEYISGENLER.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class _2_DEYISGENLER {
+public class _2_DEYISGENLER {
+
+    public static void main(String[] args) {
+
+        /*burda yaradacagimiz deyisgenin (yani icine datamizi yigacagimiz qutu kimi tesevvur edek )
+        tipini yaziriq(string ve ya integer kimi ) sonra deyisgenimize ad veririk (reqem, metn).
+        sonra ise yaratdigimiz deyisgenin icine datamizi qoyuruq (12345 ,hey!)
+        */
+
+        int reqem = 12345;
+        String metn= "hey!";
+        System.out.println(reqem);
+        System.out.println(metn);
+
+        System.out.println(reqem);
+        System.out.println(metn);
+
+        System.out.println(reqem);
+        System.out.println(metn);
+
+
+        /*
+        deyisgenin avantaji odur ki meselen bele coxlu coxlu println ler yazmisiq hamisina 12345
+        yazmisiq icine ve biri gelib dese ki onlari deyismek lazimdi hamsin 54321 kimi yazmaq
+        lazimdi , onda yulerle println i uzun kodda secib el ile manual deyismeli olaciq ,
+        amma deyisgendedi deye  onu sadece deyisgen icinde deyissek onda avtomatik butun printlerde
+        avtomatik deyisecek .
+         int reqem = 54321 ; kimi deyisilir yani
+         */
+
+//sout sozun yazib tab basanda onda avtomatik  system out println sozun yazir (shotcut key)
+        System.out.println("hello :) ");
+
+    }
 }
