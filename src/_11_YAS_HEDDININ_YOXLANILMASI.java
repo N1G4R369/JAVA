fd6c748893a01a34065b3e23b3be548d807487fc
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-09-28 15:43:20 +0400

    commit

---
+++ src/_11_YAS_HEDDININ_YOXLANILMASI.java
@@ -0,0 +1,34 @@
+import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class _11_YAS_HEDDININ_YOXLANILMASI {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("tevvelludunuzu yazin : ");
+        int tevvellud = input.nextInt();
+
+        // burda userin yazdigi tevvelude gore onun yasini cixarir (sysdate - tevvelud funksiyasi ile)
+       // buradan
+        Date inputs=new Date();
+        int year=inputs.getYear();
+        int currentYear=year+1900;
+        System.out.println("hazirda olduugumuz il budur  : "+currentYear);
+         int yas = currentYear - tevvellud;
+        System.out.println("sizin yasiniz  : " + yas );
+        //bura qeder olan hisse internetde taodigim kodda etdiyim deyisikle yazilib.
+        // kursdaki dersde manual yazirdi oldugumuz ili(2022 kimi) men ise sysdate kimi deyisdim
+        // bir il kecse de eyni netice isle avtomatik islesin deye
+
+        if (yas >=18) {
+            System.out.println("yas heddiniz uygundur sendeler ucun ");
+
+        }
+
+        else {
+            System.out.println("yas hediniz senedler verilmesi ucun uygun deyildir");
+        }
+    }
+}
