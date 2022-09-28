b17fef93fdae702cc5818c3c54fb8d122588fbb1
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-09-28 15:13:17 +0400

    commit

--- src/_10_IF_ELSE_SERTI.java
+++ src/_10_IF_ELSE_SERTI.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class _10_IF_ELSE_SERTI {
     public static void main(String[] args) {
 
+        System.out.println("-----------------------birinci  numune ------------------------");
 
         ///birinci numune
 
@@ -25,6 +26,7 @@ public class _10_IF_ELSE_SERTI {
         System.out.println(" ");
         System.out.println(" ");
 
+        System.out.println("-----------------------ikinci numune ------------------------");
 
         ///ikinci numune
         Scanner scanners = new Scanner (System.in);
@@ -44,6 +46,35 @@ public class _10_IF_ELSE_SERTI {
         else {
             System.out.println("qazanmadiniz ");
         }
+
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+
+
+        System.out.println("-----------------------ucuncu numune ------------------------");
+        /// ucuncu nummune
+
+
+        Scanner my_input= new Scanner(System.in);
+        System.out.println("reqem yazin : ");
+        int reqem10 = my_input.nextInt();
+
+        if (reqem10 >100){
+            reqem10=reqem10/2;
+        }
+
+        else if(reqem10==100){
+            //burda bos qoyuruq yani reqem 100 e beraberdise hecne eleme sadece reqemin ozun cixar ekrana , o funksiya da en  asagida printde var
+        }
+        else {
+            reqem10=reqem10*2;
+        }
+        System.out.println(reqem10);
+
+
 
     }
 }
