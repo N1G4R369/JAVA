5c86a67a95e1654a0bd888df968234c7b1befdb2
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-09-28 14:09:08 +0400

    commit

---
+++ src/_9_QIYMET_HESABLAMA.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class _9_QIYMET_HESABLAMA {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nece eded komputer almaq isteyirsiniz ? :");
+        int komputer = scanner.nextInt();
+        int komputer_pulu = 3000 * komputer;
+
+        System.out.println("nece eded server almaq isteyirsiniz ? :");
+        int server = scanner.nextInt();
+        int server_pulu = server*8000;
+
+        System.out.println("nece eded network switch almaq isteyirsiniz ? :");
+        int switches = scanner.nextInt();
+        int switches_pulu = switches*500;
+
+        int hesab= komputer_pulu+server_pulu+switches_pulu;
+        System.out.println(" umumi odemeli oldugunuz mebleq : " + hesab + " azndir");
+
+    }
+}
