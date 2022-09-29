3526e44b6da341b3a54d191f3c071cae3d6b1aa8
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-09-29 12:32:24 +0400

    commit

---
+++ src/username/_14_USERNAME_VE_PAROL_YOXLAMA.java
@@ -0,0 +1,65 @@
+package username;
+
+import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
+
+import java.util.Scanner;
+
+public class _14_USERNAME_VE_PAROL_YOXLAMA {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("username yazin : ");
+        String username = input.nextLine();
+
+        System.out.println("password yazin : ");
+        String password = input.nextLine();
+
+        String user1="ADMIN";
+
+        String password1="123456";
+
+        if (username.equals(user1))
+        {
+            System.out.println("username duzdu");
+            if (password.equals(password1))
+            {
+                System.out.println("parol duzdu");
+                System.out.println("sisteme daxil oldunuz");
+            }
+
+        else {
+                System.out.println("parol sehvdi");
+            }
+
+        }
+
+        else {
+            System.out.println("user ve parol sehvdir ");
+            System.out.println("sistemden cixis edilir");
+        }
+
+
+        System.out.println( " ");
+        System.out.println( "--------- 2 ci alternativ -------------");
+        System.out.println( " ");
+
+        ///// ALTERNATIVI ( EYNI NETICE ILE ISLEYIR)
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("username yazin : ");
+        String username2= scanner.nextLine();
+
+        System.out.println("passwordunuzu yazin : ");
+        String password2 = scanner.nextLine();
+
+        if (username2.equals("ADMIN") && password2.equals("123456"))
+        {
+            System.out.println("sisteme girdiniz");
+        }
+         else
+        {
+            System.out.println("giris ugursuzdur ");
+        }
+
+    }
+}
