5d0b47731870af8ac2887b61cb475524902961a3
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-09-30 10:49:29 +0400

    commit

---
+++ src/test.java
@@ -0,0 +1,16 @@
+public class test {
+
+
+    public static void main(String[] args) {
+        int sayi =20;
+        sayi=sayi % 5;
+        if(sayi ==0)
+        {
+            System.out.println("Bölünüyor");
+        }
+        else {
+
+        }
+
+    }
+}
