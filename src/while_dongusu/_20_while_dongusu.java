303907d8fcc5ad2fbc4650339167b88687cb81a3
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-10-03 15:54:33 +0400

    commit

---
+++ src/while_dongusu/_20_while_dongusu.java
@@ -0,0 +1,16 @@
+package while_dongusu;
+
+/* for  dongusu ile eyni mentiqdi 10 defe ekrana nigar irilova yazisin cixarir */
+public class _20_while_dongusu {
+    public static void main(String[] args) {
+        int i = 0 ;
+        while (i < 10)
+        {
+            System.out.println("nigar kirilova ");
+            i = i+1;
+        }
+
+        System.out.println("loop bitdi ");
+        }
+    }
+
