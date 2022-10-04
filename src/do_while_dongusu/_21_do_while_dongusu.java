659fbbceaa397450e4d0838d65312d4781f85cc9
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-10-04 11:43:30 +0400

    commit

---
+++ src/do_while_dongusu/_21_do_while_dongusu.java
@@ -0,0 +1,14 @@
+package do_while_dongusu;
+// buda for ve sade while dongusu ile eyni mentiqdi sade burada emri birinci verirsen(do) ve seri sonrasina qoyursan
+
+public class _21_do_while_dongusu {
+    public static void main(String[] args) {
+        int i =0;
+        do {
+            System.out.println("NigarKirilova");
+            i++;
+        }
+        while(i <10);
+        System.out.println("loop bitdi ");
+    }
+}
