1020fe75c3ac818aad66cfee536f0129ad330de4
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-10-13 11:33:12 +0400

    commit

--- src/Arraylar/ededler_yazib_array_icinde_muqaiseler.java
+++ src/Arraylar/ededler_yazib_array_icinde_muqaiseler.java
@@ -7,19 +7,32 @@ public class ededler_yazib_array_icinde_muqaiseler {
         Scanner input =new Scanner(System.in);
         int[] reqemler = new int[10];
         int ilk_son_toplama=0;
+        int umumiToplam=0;
 
         for (int i = 1; i<=5;i++)
         {
             System.out.println(i+".ci ededi yazin");
              reqemler[i]=input.nextInt();
-             System.out.println(reqemler[i]);
 
+             umumiToplam+=reqemler[i];
+            System.out.println("umumi toplama : "+reqemler[i] );
 
 
         }
-        ilk_son_toplama=reqemler[1]+reqemler[5];
-        System.out.println("toplam : "+ilk_son_toplama);
 
+
+        System.out.println("umumi toplama : "+ umumiToplam );
+
+        ilk_son_toplama=reqemler[1]+reqemler[5];
+        System.out.println("ilktoplam : "+ilk_son_toplama);
+
+        double ortalama =+ ilk_son_toplama/umumiToplam;
+        System.out.println("ortalama qiymeti : "+ortalama );
 
     }
 }
+
+
+/*
+ burda da umumi toplayir userin yazdigi 5 reqemi , sonra ilk ve son reqemleri toplayir,en sonda da ortalasin tapit
+ */
