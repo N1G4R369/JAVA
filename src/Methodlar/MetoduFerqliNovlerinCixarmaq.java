fc2b9d0025b87407d90a7f5493a7e37ba005ac99
NIGAR ALIZADA <106949962+N1G4R369@users.noreply.github.com>
2022-10-19 17:05:28 +0400

    commit

--- src/Methodlar/MetoduFerqliNovlerinCixarmaq.java
+++ src/Methodlar/MetoduFerqliNovlerinCixarmaq.java
@@ -13,13 +13,34 @@ public class MetoduFerqliNovlerinCixarmaq {
         System.out.println(ortalama);
     }
 
+  /* burda her iki metodun adi eynidi , amma funksiyalari ferqlidi cunki birinci ortalama cixarma metodunda 2 ededin ortalamasin cixarir ,
+        2 ci de ise 3 ededin ortalamasini cixarir  ve bele emeliyata pverloadin emeliyati deyirler*/
+
+
+    public static void kvadratinAlmaq (double reqem) {
+        double netice = reqem *reqem;
+        System.out.println(netice);
+    }
+
+    public static void kvadratinAlmaq (int reqem) {
+        double netice = reqem *reqem;
+        System.out.println(netice);
+    }
+
+
+
+    /* burda da */
+
     public static void main(String[] args) {
        ortalamaCixarmaq(20,30,40);
        ortalamaCixarmaq(50,60);
 
-       /* burda her iki metodun adi eynidi , amma funksiyalari ferqlidi cunki birinci ortalama cixarma metodunda 2 ededin ortalamasin cixarir ,
-        2 ci de ise 3 ededin ortalamasini cixarir  ve bele emeliyata pverloadin emeliyati deyirler*/
+
+
 
 
     }
+
+
+
 }
